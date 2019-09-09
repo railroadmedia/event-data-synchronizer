f32c11f1bf15f6d33fd3412cf24d846735d849e9
calebfavor <calebfavor@gmail.com>
2019-09-09 11:34:45 -0700

    Adding intercom subscription sync logic.

--- src/Listeners/Intercom/IntercomSyncEventListener.php
+++ src/Listeners/Intercom/IntercomSyncEventListener.php
@@ -4,6 +4,7 @@ namespace Railroad\EventDataSynchronizer\Listeners\Intercom;
 
 use Carbon\Carbon;
 use Railroad\Ecommerce\Entities\PaymentMethod;
+use Railroad\Ecommerce\Entities\Subscription;
 use Railroad\Ecommerce\Events\PaymentMethods\PaymentMethodCreated;
 use Railroad\Ecommerce\Events\PaymentMethods\PaymentMethodUpdated;
 use Railroad\Ecommerce\Events\Subscriptions\SubscriptionCreated;
@@ -48,6 +49,7 @@ class IntercomSyncEventListener
     private $userPaymentMethodsRepository;
 
     protected const USER_ID_PREFIX = 'musora_';
+    protected const BRANDS_TO_SYNC = ['pianote', 'guitareo'];
 
     public function __construct(
         IntercomeoService $intercomeoService,
@@ -231,9 +233,143 @@ class IntercomSyncEventListener
         );
     }
 
+    /**
+     * @param $userId
+     */
     public function syncUserMembershipAndProductData($userId)
     {
         $userSubscriptions = $this->subscriptionRepository->getAllUsersSubscriptions($userId);
         $userProducts = $this->userProductRepository->getAllUsersProducts($userId);
+
+        $finalCustomAttributes = [];
+
+        foreach (self::BRANDS_TO_SYNC as $brand) {
+
+            // subscription related attributes
+            $finalCustomAttributes = array_merge(
+                $finalCustomAttributes,
+                $this->getSubscriptionAttributes($userSubscriptions, $brand)
+            );
+        }
+
+        if (!empty($finalCustomAttributes)) {
+            dispatch(
+                new IntercomSyncUser(
+                    self::USER_ID_PREFIX . $userId, [
+                        'custom_attributes' => $finalCustomAttributes,
+                    ]
+                )
+            );
+        }
+
+    }
+
+
+    // pianote_completed_2018_foundations_learning_path
+    // pianote_user
+    // pianote_birthday (PDT)
+    // pianote_primary_payment_method_expiration_date (PDT)
+    // pianote_membership_access_expiration_date (PDT)
+    // pianote_membership_subscription_status
+    // pianote_membership_subscription_renewal_date (PDT)
+    // pianote_membership_subscription_type
+    // pianote_membership_subscription_cancellation_date (PDT)
+    // pianote_membership_subscription_started_date (PDT)
+    // pianote_500_songs_in_5_days_pack_owner
+    // pianote_completed_2016_foundations_learning_path
+
+
+    /**
+     * @param Subscription[] $userSubscriptions
+     * @param $brand
+     * @return array
+     */
+    private function getSubscriptionAttributes(array $userSubscriptions, $brand)
+    {
+        /**
+         * @var $subscriptionToSync Subscription|null
+         */
+
+        // Subscriptions:
+        // We only want to sync attributes to subscriptions for membership products.
+        // If there are multiple membership subs we will always sync the one with the further paid_until
+        // in the future.
+        $subscriptionToSync = null;
+
+        foreach ($userSubscriptions as $userSubscription) {
+            // make sure this subscription is for the brand being processed
+            if ($userSubscription->getBrand() !== $brand) {
+                continue;
+            }
+
+            // make sure the subscriptions product is in this brands pre-configured products that represent a membership
+            if (!in_array(
+                $userSubscription->getProduct()
+                    ->getId(),
+                config(
+                    'event-data-synchronizer.' .
+                    $userSubscription->getProduct()
+                        ->getBrand() .
+                    '_membership_product_ids'
+                )
+            )) {
+                continue;
+            }
+
+            if (empty($subscriptionToSync)) {
+                $subscriptionToSync = $userSubscription;
+
+                continue;
+            }
+
+            // if this subscription paid_until is further in the past than whatever is currently set, skip it
+            // unless the set subscription is not-active and this one is, then use the active one
+            if (!empty($subscriptionToSync) &&
+                ($subscriptionToSync->getPaidUntil() < $userSubscription->getPaidUntil() ||
+                    !$subscriptionToSync->getIsActive() &&
+                    $userSubscription->getIsActive())) {
+                $subscriptionToSync = $userSubscription;
+            }
+        }
+
+        if (!empty($subscriptionToSync)) {
+
+            $totalPaymentsOnActiveSubscription = 0;
+
+            foreach ($subscriptionToSync->getPayments() as $_payment) {
+                if ($_payment->getTotalPaid() == $_payment->getTotalDue()) {
+                    $totalPaymentsOnActiveSubscription++;
+                }
+            }
+
+            $membershipRenewalDate = $subscriptionToSync->getPaidUntil()->timestamp;
+            $membershipCancellationDate = $subscriptionToSync->getCanceledOn();
+            $subscriptionStatus = $subscriptionToSync->getState();
+            $subscriptionStartedDate = $subscriptionToSync->getCreatedAt()->timestamp;
+
+        }
+        else {
+            $membershipRenewalDate = null;
+            $paymentMethodExpirationDate = null;
+            $membershipCancellationDate = null;
+            $subscriptionStatus = null;
+            $subscriptionStartedDate = null;
+        }
+
+        $subscriptionProductTag = null;
+
+        if (!empty($subscriptionToSync)) {
+            $subscriptionProductTag =
+                $subscriptionToSync->getIntervalCount() . '_' . $subscriptionToSync->getIntervalType();
+
+        }
+
+        return [
+            $brand . '_membership_subscription_status' => $subscriptionStatus,
+            $brand . '_membership_subscription_type' => $subscriptionProductTag,
+            $brand . '_membership_subscription_renewal_date' => $membershipRenewalDate,
+            $brand . '_membership_subscription_cancellation_date' => $membershipCancellationDate,
+            $brand . '_membership_subscription_started_date' => $subscriptionStartedDate,
+        ];
     }
 }
