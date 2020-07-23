0ed774daf7507aa57fbb9308f6aab02ffb434197
Roxana Riza <roxana_r2000@yahoo.com>
2020-07-23 13:18:18 +0300

    Update to Railcontent 1.0

--- src/Listeners/UserProductToUserContentPermissionListener.php
+++ src/Listeners/UserProductToUserContentPermissionListener.php
@@ -8,12 +8,13 @@ use Railroad\Ecommerce\Events\UserProducts\UserProductDeleted;
 use Railroad\Ecommerce\Events\UserProducts\UserProductUpdated;
 use Railroad\Ecommerce\Repositories\ProductRepository;
 use Railroad\Ecommerce\Repositories\UserProductRepository;
-use Railroad\Railcontent\Helpers\CacheHelper;
-use Railroad\Railcontent\Repositories\PermissionRepository;
-use Railroad\Railcontent\Repositories\UserPermissionsRepository;
-use Railroad\Railcontent\Services\ConfigService;
+use Railroad\Railcontent\Entities\Content;
+use Railroad\Railcontent\Entities\Permission;
+use Railroad\Railcontent\Entities\UserPermission;
+use Railroad\Railcontent\Managers\RailcontentEntityManager;
 use Railroad\Resora\Events\Created;
 use Railroad\Resora\Events\Updated;
+use Railroad\Usora\Repositories\UserRepository;
 
 class UserProductToUserContentPermissionListener
 {
@@ -28,34 +29,47 @@ class UserProductToUserContentPermissionListener
     private $productRepository;
 
     /**
-     * @var PermissionRepository
+     * @var \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
      */
     private $permissionRepository;
 
     /**
-     * @var UserPermissionsRepository
+     * @var \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
      */
     private $userPermissionsRepository;
 
     /**
-     * UserSubscriptionUserToContentPermissionListener constructor.
+     * @var RailcontentEntityManager
+     */
+    private $railcontentEntityManager;
+
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * UserProductToUserContentPermissionListener constructor.
      *
      * @param UserProductRepository $userProductRepository
      * @param ProductRepository $productRepository
-     * @param PermissionRepository $permissionRepository
-     * @param UserPermissionsRepository $userPermissionsRepository
+     * @param RailcontentEntityManager $railcontentEntityManager
+     * @param UserRepository $userRepository
      */
     public function __construct(
         UserProductRepository $userProductRepository,
         ProductRepository $productRepository,
-        PermissionRepository $permissionRepository,
-        UserPermissionsRepository $userPermissionsRepository
-    )
-    {
+        RailcontentEntityManager $railcontentEntityManager,
+        UserRepository $userRepository
+    ) {
+        $this->railcontentEntityManager = $railcontentEntityManager;
         $this->userProductRepository = $userProductRepository;
         $this->productRepository = $productRepository;
-        $this->permissionRepository = $permissionRepository;
-        $this->userPermissionsRepository = $userPermissionsRepository;
+        $this->userRepository = $userRepository;
+
+        $this->permissionRepository = $this->railcontentEntityManager->getRepository(Permission::class);
+        $this->userPermissionsRepository = $this->railcontentEntityManager->getRepository(UserPermission::class);
+
     }
 
     /**
@@ -63,7 +77,11 @@ class UserProductToUserContentPermissionListener
      */
     public function handleCreated(UserProductCreated $createdEvent)
     {
-        $this->syncUserId($createdEvent->getUserProduct()->getUser()->getId());
+        $this->syncUserId(
+            $createdEvent->getUserProduct()
+                ->getUser()
+                ->getId()
+        );
     }
 
     /**
@@ -71,7 +89,11 @@ class UserProductToUserContentPermissionListener
      */
     public function handleUpdated(UserProductUpdated $updatedEvent)
     {
-        $this->syncUserId($updatedEvent->getNewUserProduct()->getUser()->getId());
+        $this->syncUserId(
+            $updatedEvent->getNewUserProduct()
+                ->getUser()
+                ->getId()
+        );
     }
 
     /**
@@ -79,7 +101,11 @@ class UserProductToUserContentPermissionListener
      */
     public function handleDeleted(UserProductDeleted $deletedEvent)
     {
-        $this->syncUserId($deletedEvent->getUserProduct()->getUser()->getId());
+        $this->syncUserId(
+            $deletedEvent->getUserProduct()
+                ->getUser()
+                ->getId()
+        );
     }
 
     public function syncUserId($userId)
@@ -89,19 +115,22 @@ class UserProductToUserContentPermissionListener
         $permissionsToCreate = [];
 
         foreach ($allUsersProducts as $allUsersProduct) {
-            $permissionName =
-                config(
-                    'event-data-synchronizer.ecommerce_product_sku_to_content_permission_name_map.' .
-                    $allUsersProduct->getProduct()
-                        ->getSku()
-                );
+            $permissionName = config(
+                'event-data-synchronizer.ecommerce_product_sku_to_content_permission_name_map.' .
+                $allUsersProduct->getProduct()
+                    ->getSku()
+            );
 
             if (empty($permissionName)) {
                 continue;
             }
 
             // we need to check by brand as well since some permissions across brands have the same name
-            $permissionArrayKey = $permissionName . '|' . $allUsersProduct->getProduct()->getBrand();
+            $permissionArrayKey =
+                $permissionName .
+                '|' .
+                $allUsersProduct->getProduct()
+                    ->getBrand();
 
             if (!array_key_exists($permissionArrayKey, $permissionsToCreate)) {
                 $permissionsToCreate[$permissionArrayKey] = $allUsersProduct->getExpirationDate();
@@ -120,58 +149,49 @@ class UserProductToUserContentPermissionListener
             $permissionNameToSync = explode('|', $permissionNameAndBrandToSync)[0];
             $permissionBrandToSync = explode('|', $permissionNameAndBrandToSync)[1];
 
-            $permissionId =
-                $this->permissionRepository->query()
-                    ->where('name', $permissionNameToSync)
-                    ->where('brand', $permissionBrandToSync)
-                    ->first(['id'])['id'] ?? null;
+            $permission =
+                $this->permissionRepository->createQueryBuilder('p')
+                    ->where('p.name = :name')
+                    ->andWhere('p.brand = :brand')
+                    ->setParameter('name', $permissionNameToSync)
+                    ->setParameter('brand', $permissionBrandToSync)
+                    ->getQuery()
+                    ->getOneOrNullResult();
 
-            if (empty($permissionId)) {
+            if (!$permission) {
                 continue;
             }
 
-            $existingPermission =
-                $this->userPermissionsRepository->getIdByPermissionAndUser(
-                    $userId,
-                    $permissionId
-                )[0] ?? null;
+            $existingPermission = $this->userPermissionsRepository->userPermission($userId, $permission);
 
-            if (!empty($expirationDate)) {
-                $expirationDate = $expirationDate->toDateTimeString();
-            }
+            if (!($existingPermission)) {
 
-            if (empty($existingPermission)) {
-                $this->userPermissionsRepository->create(
-                    [
-                        'user_id' => $userId,
-                        'permission_id' => $permissionId,
-                        'start_date' => Carbon::now()
-                            ->toDateTimeString(),
-                        'expiration_date' => $expirationDate,
-                        'created_on' => Carbon::now()
-                            ->toDateTimeString(),
-                    ]
+                $user = $this->userRepository->find($userId);
+
+                $existingPermission = new UserPermission();
+                $existingPermission->setUser($user);
+                $existingPermission->setPermission($permission);
+                $existingPermission->setStartDate(
+                    Carbon::now()
+                        ->toDateTimeString()
                 );
+                $existingPermission->setExpirationDate($expirationDate);
+                $existingPermission->setCreatedAt(Carbon::now());
+
+                $this->railcontentEntityManager->persist($existingPermission);
+                $this->railcontentEntityManager->flush();
+
             } else {
-                $this->userPermissionsRepository->update(
-                    $existingPermission['id'],
-                    [
-                        'user_id' => $userId,
-                        'permission_id' => $permissionId,
-                        'expiration_date' => $expirationDate,
-                        'updated_on' => Carbon::now()
-                            ->toDateTimeString(),
-                    ]
-                );
+
+                $existingPermission->setExpirationDate($expirationDate);
+
+                $this->railcontentEntityManager->persist($existingPermission);
+                $this->railcontentEntityManager->flush();
             }
         }
 
-        // clear the railcontent cache
-        CacheHelper::deleteUserFields(
-            [
-                ConfigService::$redisPrefix . ':userId_' . $userId,
-            ],
-            'content'
-        );
+        // clear the cache
+        $this->railcontentEntityManager->getCache()
+            ->evictEntityRegion(Content::class);
     }
 }
