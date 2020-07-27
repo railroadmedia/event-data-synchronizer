c363eaa906dc5867b7b07e30f89ccf369ce709dc
calebfavor <caleb@drumeo.com>
2020-07-27 14:56:03 -0700

    Bug fix.

--- src/Listeners/UserProductToUserContentPermissionListener.php
+++ src/Listeners/UserProductToUserContentPermissionListener.php
@@ -120,7 +120,7 @@ class UserProductToUserContentPermissionListener
             } elseif (isset($permissionsToCreate[$permissionArrayKey]) &&
                 $permissionsToCreate[$permissionArrayKey] !== null) {
 
-                if (Carbon::parse($permissionsToCreate[$permissionArrayKey]) < $allUsersProduct->getExpirationDate()) {
+                if ($permissionsToCreate[$permissionArrayKey]['expiration_date'] < $allUsersProduct->getExpirationDate()) {
                     $permissionsToCreate[$permissionArrayKey] = [
                         'expiration_date' => $allUsersProduct->getExpirationDate(),
                         'start_date' => $allUsersProduct->getStartDate(),
