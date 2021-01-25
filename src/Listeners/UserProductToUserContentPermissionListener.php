e4923b40fd0a04c589b552b523a58e2b413553ca
calebfavor <caleb@drumeo.com>
2021-01-25 11:41:26 -0800

    Removing error log debugging for permissions syncing, no longer needed the issue is fixed.

--- src/Listeners/UserProductToUserContentPermissionListener.php
+++ src/Listeners/UserProductToUserContentPermissionListener.php
@@ -137,9 +137,6 @@ class UserProductToUserContentPermissionListener
             $expirationDate = $dates['expiration_date'];
             $startDate = $dates['start_date'] ?? Carbon::now()->toDateTimeString();
 
-            error_log('$permissionNameToSync - ' . $permissionNameToSync);
-            error_log('$permissionBrandToSync - ' . $permissionBrandToSync);
-
             $permissionId =
                 $this->permissionRepository->query()
                     ->where('name', $permissionNameToSync)
