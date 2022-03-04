41c25447400b86d88de215e4e61b6ebe711da1dc
calebfavor <calebfavor@gmail.com>
2022-03-04 14:59:17 -0800

    Turning off failed customer.io events trying to re-sync the user first. I think this may be what is causing the dupes. Needs review after a day or 2.

--- src/Jobs/CustomerIoCreateEventByUserId.php
+++ src/Jobs/CustomerIoCreateEventByUserId.php
@@ -120,7 +120,7 @@ class CustomerIoCreateEventByUserId extends CustomerIoBaseJob
         error_log($exception);
 
         // try to sync the user first, then retry
-        event(new UserCreated($user));
+//        event(new UserCreated($user));
 
         if ($this->job->attempts() >= $this->tries) {
             $this->fail($exception);
