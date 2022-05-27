3d02eb4dda77f29407e725383d79ca384e3ebdc1
calebfavor <calebfavor@gmail.com>
2022-05-27 15:21:35 -0700

    Finishing up tests for membership logic syncing.

--- tests/Functional/UserMembershipFieldsServiceTest.php
+++ tests/Functional/UserMembershipFieldsServiceTest.php
@@ -41,7 +41,6 @@ class UserMembershipFieldsServiceTest extends EventDataSynchronizerTestCase
         ]);
     }
 
-
     public function test_sync_no_user_products()
     {
         $userId = $this->createUserAndGetId();
@@ -477,4 +476,166 @@ class UserMembershipFieldsServiceTest extends EventDataSynchronizerTestCase
             'access_level' => 'house-coach',
         ]);
     }
+
+    public function test_sync_recurring_monthly_membership_access_ignore_old_user_products()
+    {
+        $userId = $this->createUserAndGetId();
+
+        $user = new User($userId, $this->faker->email);
+
+        $product = new Product();
+
+        $product->setBrand($this->faker->word);
+        $product->setName($this->faker->word);
+        $product->setSku($this->faker->word);
+        $product->setPrice($this->faker->randomNumber(4));
+        $product->setType(Product::TYPE_DIGITAL_SUBSCRIPTION);
+        $product->setDigitalAccessType(Product::DIGITAL_ACCESS_TYPE_ALL_CONTENT_ACCESS);
+        $product->setDigitalAccessTimeType(Product::DIGITAL_ACCESS_TIME_TYPE_RECURRING);
+        $product->setDigitalAccessTimeIntervalType(Product::DIGITAL_ACCESS_TIME_INTERVAL_TYPE_MONTH);
+        $product->setDigitalAccessTimeIntervalLength(1);
+        $product->setActive(true);
+        $product->setIsPhysical(false);
+        $product->setAutoDecrementStock(false);
+        $product->setCreatedAt(Carbon::now());
+
+        $this->ecommerceEntityManager->persist($product);
+
+        $userProduct1 = new UserProduct();
+
+        $userProduct1->setUser($user);
+        $userProduct1->setProduct($product);
+        $userProduct1->setQuantity(1);
+        $userProduct1->setExpirationDate(Carbon::now()->subDays(25));
+        $userProduct1->setStartDate(Carbon::now()->subDay());
+        $userProduct1->setCreatedAt(Carbon::now());
+
+        $this->ecommerceEntityManager->persist($userProduct1);
+
+        $userProduct2 = new UserProduct();
+
+        $userProduct2->setUser($user);
+        $userProduct2->setProduct($product);
+        $userProduct2->setQuantity(1);
+        $userProduct2->setExpirationDate(Carbon::now()->addDay());
+        $userProduct2->setStartDate(Carbon::now()->subDay());
+        $userProduct2->setCreatedAt(Carbon::now());
+
+        $this->ecommerceEntityManager->persist($userProduct2);
+
+        $userProduct3 = new UserProduct();
+
+        $userProduct3->setUser($user);
+        $userProduct3->setProduct($product);
+        $userProduct3->setQuantity(1);
+        $userProduct3->setExpirationDate(Carbon::now()->addDays(43));
+        $userProduct3->setStartDate(Carbon::now()->subDay());
+        $userProduct3->setCreatedAt(Carbon::now());
+
+        $this->ecommerceEntityManager->persist($userProduct3);
+        $this->ecommerceEntityManager->flush();
+
+        $this->userMembershipFieldsService->sync($userId);
+
+        $this->assertDatabaseHas('usora_users', [
+            'membership_expiration_date' => $userProduct3->getExpirationDate(),
+            'is_lifetime_member' => false,
+            'access_level' => 'member',
+        ]);
+    }
+
+    public function test_sync_lifetime_membership_access_ignore_old_user_products()
+    {
+        $userId = $this->createUserAndGetId();
+
+        $user = new User($userId, $this->faker->email);
+
+        $product = new Product();
+
+        $product->setBrand($this->faker->word);
+        $product->setName($this->faker->word);
+        $product->setSku($this->faker->word);
+        $product->setPrice($this->faker->randomNumber(4));
+        $product->setType(Product::TYPE_DIGITAL_SUBSCRIPTION);
+        $product->setDigitalAccessType(Product::DIGITAL_ACCESS_TYPE_ALL_CONTENT_ACCESS);
+        $product->setDigitalAccessTimeType(Product::DIGITAL_ACCESS_TIME_TYPE_RECURRING);
+        $product->setDigitalAccessTimeIntervalType(Product::DIGITAL_ACCESS_TIME_INTERVAL_TYPE_MONTH);
+        $product->setDigitalAccessTimeIntervalLength(1);
+        $product->setActive(true);
+        $product->setIsPhysical(false);
+        $product->setAutoDecrementStock(false);
+        $product->setCreatedAt(Carbon::now());
+
+        $this->ecommerceEntityManager->persist($product);
+
+        $lifetimeProduct = new Product();
+
+        $lifetimeProduct->setBrand($this->faker->word);
+        $lifetimeProduct->setName($this->faker->word);
+        $lifetimeProduct->setSku($this->faker->word);
+        $lifetimeProduct->setPrice($this->faker->randomNumber(4));
+        $lifetimeProduct->setType(Product::TYPE_DIGITAL_SUBSCRIPTION);
+        $lifetimeProduct->setDigitalAccessType(Product::DIGITAL_ACCESS_TYPE_ALL_CONTENT_ACCESS);
+        $lifetimeProduct->setDigitalAccessTimeType(Product::DIGITAL_ACCESS_TIME_TYPE_LIFETIME);
+        $lifetimeProduct->setActive(true);
+        $lifetimeProduct->setIsPhysical(false);
+        $lifetimeProduct->setAutoDecrementStock(false);
+        $lifetimeProduct->setCreatedAt(Carbon::now());
+
+        $this->ecommerceEntityManager->persist($lifetimeProduct);
+
+        $userProduct1 = new UserProduct();
+
+        $userProduct1->setUser($user);
+        $userProduct1->setProduct($product);
+        $userProduct1->setQuantity(1);
+        $userProduct1->setExpirationDate(Carbon::now()->subDays(25));
+        $userProduct1->setStartDate(Carbon::now()->subDay());
+        $userProduct1->setCreatedAt(Carbon::now());
+
+        $this->ecommerceEntityManager->persist($userProduct1);
+
+        $userProduct2 = new UserProduct();
+
+        $userProduct2->setUser($user);
+        $userProduct2->setProduct($product);
+        $userProduct2->setQuantity(1);
+        $userProduct2->setExpirationDate(Carbon::now()->addDay());
+        $userProduct2->setStartDate(Carbon::now()->subDay());
+        $userProduct2->setCreatedAt(Carbon::now());
+
+        $this->ecommerceEntityManager->persist($userProduct2);
+
+        $userProduct3 = new UserProduct();
+
+        $userProduct3->setUser($user);
+        $userProduct3->setProduct($product);
+        $userProduct3->setQuantity(1);
+        $userProduct3->setExpirationDate(Carbon::now()->addDays(43));
+        $userProduct3->setStartDate(Carbon::now()->subDay());
+        $userProduct3->setCreatedAt(Carbon::now());
+
+        $this->ecommerceEntityManager->persist($userProduct3);
+
+        $userProduct4 = new UserProduct();
+
+        $userProduct4->setUser($user);
+        $userProduct4->setProduct($lifetimeProduct);
+        $userProduct4->setQuantity(1);
+        $userProduct4->setExpirationDate(null);
+        $userProduct4->setStartDate(Carbon::now()->subDay());
+        $userProduct4->setCreatedAt(Carbon::now());
+
+        $this->ecommerceEntityManager->persist($userProduct4);
+        $this->ecommerceEntityManager->flush();
+
+        $this->userMembershipFieldsService->sync($userId);
+
+        $this->assertDatabaseHas('usora_users', [
+            'membership_expiration_date' => null,
+            'is_lifetime_member' => true,
+            'access_level' => 'lifetime',
+        ]);
+    }
+
 }
