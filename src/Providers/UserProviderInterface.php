0f8688bc68239b0ac388f3381dfaf1feabd27453
calebfavor <calebfavor@gmail.com>
2023-01-05 11:09:24 -0800

    Adding new syncing for membership level column

--- src/Providers/UserProviderInterface.php
+++ src/Providers/UserProviderInterface.php
@@ -14,17 +14,19 @@ interface UserProviderInterface
 
     /**
      * @param int $userId
-     * @param Carbon $membershipExpirationDate
+     * @param Carbon|null $membershipExpirationDate
      * @param bool $isLifetimeMember
      * @param string $accessLevel
      * @param bool $isPackOwner
+     * @param string|null $membershipLevel
      * @return bool
      */
     public function saveMembershipData(
         int $userId,
-        Carbon $membershipExpirationDate,
+        Carbon $membershipExpirationDate = null,
         bool $isLifetimeMember,
         string $accessLevel,
-        bool $isPackOwner
+        bool $isPackOwner,
+        string $membershipLevel = null
     ): bool;
 }
